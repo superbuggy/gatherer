c65c1ae8edbc1db0061068d594d53b267b6ee0ed
superbuggy <jpqreichard@gmail.com>
2017-05-08 17:06:12 -0400

    add cors

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const getMtgJson = require('mtg-json');
-const app = require('express')();
-const data = require('./AllCards.json');
+const  getMtgJson = require('mtg-json')
+
+const  express = require('express')
+const  app = express()
+const  data = require('./AllCards.json')
+const  cors = require('cors')();
+
+app.use( express.static('views') )
+app.use( cors )
 
 //Request AllCards.json and store it in the current directory
-app.get("/grabem", (req, res) => {
-  getMtgJson('cards', __dirname).then(json => {
-    console.log(json) // Logs ['Creature']
-  });
-  res.send({success:true})
+app.get("/pulljson", (req, res) => {
+  getMtgJson('cards', __dirname)
+    .then(json => {
+      console.log(json)
+      res.send({success: true})
+    })
+    .catch( err =>
+      res.send({
+        success: false,
+        err
+      }
+    ));
 })
 
-app.get("/cards?page=:page", (req, res)=>{
-  console.log(req.params.page);
-  let cards = Object.keys(data).map( cardName => data[cardName] );
-  res.send( cards )
+app.get("/cards/:id?", (req, res) => {
+  let cards = Object.keys(data).map( cardName => data[cardName] )
+  console.log(req.params.id)
+  res.json( (req.params.id) ? cards[req.params.id] : data)
 })
 
-app.get("/cards/:id", (req, res)=>{
-  let cards = Object.keys(data).map( cardName => data[cardName] );
-  res.send(cards[req.params.id]);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 })
+
 app.listen(3000)
